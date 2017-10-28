4fa64d22e53c0a400c9b2170ae70f6ad2ba9eb6b
John Chin-Jew <jchinjew@gmail.com>
2017-10-28 10:36:02 -0700

    Create conventions.lua

---
+++ conventions.lua
@@ -0,0 +1,9 @@
+-- naming and namespacing file dependencies
+SimpleShape  -- classes/closures = pascal case
+vectorUtils  -- functional modules = camel case
+game-objects -- folders = lower case dashed
+
+-- usage
+local SimpleShape = require("game-object.SimpleShape")
+local vectorUtils = require("utility.vectorUtils")
+local global = require("utility.global")
